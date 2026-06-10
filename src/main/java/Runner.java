f8f1e0f37bb2cf0747b4f136b514c1e3c740f27a
AngRod15 <angie.rodley@student.adelaide.edu.au>
2026-06-10 11:10:53 +1000

    Added the following features: Prompt user to enter the file name. Add vertices and edges to new graph Call the sorting algorithm

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -17,9 +18,17 @@ public class Runner {
     public static void main(String[] args) {
         Graph  graph = new Graph();
 
+        // initialise a scanner for user input
+        Scanner input = new Scanner(System.in);
+
         // try reading the file
         try {
-            File file = new File("XBDA.txt");
+            //File file = new File("XBDA.txt");
+            System.out.println("Please enter file name: ");
+            String fileName = input.nextLine();
+
+            File file = new File(fileName);
+
             Scanner scanner = new Scanner(file);
 
             // Read the first line of the file - this should be all the courses.
@@ -33,15 +42,49 @@ public class Runner {
                 graph.addVertex(course.trim());
             }
 
+            // move on to the next lines that contain the course and its prerequisites.
+            while (scanner.hasNextLine()) {
+
+                // Scan the next line
+                String line = scanner.nextLine();
+
+                // Split it into each course by comma
+                // The course is the first one [0]
+                String[] parts = line.split(",");
+                String course = parts[0].trim();
+
+                // Iterate through the prerequisites - start from 1
+                for (int i = 1; i < parts.length; i++) {
+                    String prerequisite = parts[i].trim();
+
+                    // Add the edge to the course.
+                    graph.addEdge(prerequisite, course);
+                }
+            }
+
+            scanner.close();
+
         } catch (FileNotFoundException e) {
             System.out.println("File not found.");
         }
 
         // Check that the vertices have been created in the graph
         System.out.println(graph.getVertices().toString());
+
+        // Check that the edges have been created
+        System.out.println(graph.getEdges().toString());
+
+        // Perform the topological sort to find the best path for visiting all the vertices.
+        List<String> sortedGraph = graph.topoSort();
+
+        System.out.println("Topologically sorted graph: ");
+
+        for (String course : sortedGraph) {
+            System.out.println(course);
+        }
+
+
+
         
     }
-
-
-    
 }
