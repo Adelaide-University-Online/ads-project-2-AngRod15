2adb069f835493702b9f89cf2970b77e36f723c9
AngRod15 <angie.rodley@student.adelaide.edu.au>
2026-06-11 14:49:45 +1000

    Implemented test for addEdge, addVertex and topoSort including exception.

--- src/main/java/GraphTest.java
+++ src/main/java/GraphTest.java
@@ -1,24 +1,87 @@
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class GraphTest {
 
+    // Create a graph to use in the tests.
+    private Graph graph;
+
+    @Before
+    public void setUp() {
+        graph = new Graph();
+    }
+
     @Test
     public void addVertex() {
+        graph.addVertex("INFT1024");
+
+            // Check that the vertex was added to the graph
+        assertTrue(graph.getVertices().containsKey("INFT1024"));
+
     }
 
     @Test
     public void addEdge() {
-    }
+        // Add two vertices and an edge
+        graph.addVertex("INFT1024");
+        graph.addVertex("INFT1026");
+        graph.addEdge("INFT1024", "INFT1026");
 
-    @Test
-    public void getVertices() {
-    }
+        // Variable to store the edge list
+        List<Edge> edgeList = graph.getEdges().get("INFT1024");
 
-    @Test
-    public void getEdges() {
+        // check that there is one entry and the to and from are correct
+        assertEquals(1, edgeList.size());
+        assertEquals("INFT1026", edgeList.get(0).getTo());
+        assertEquals("INFT1024", edgeList.get(0).getFrom());
     }
 
     @Test
     public void topoSort() {
+
+        // Add some vertices and edges to the graph
+        graph.addVertex("COMP1043");
+        graph.addVertex("COMP1048");
+        graph.addVertex("INFT1024");
+
+        graph.addEdge("COMP1043", "COMP1048");
+        graph.addEdge("COMP1048", "INFT1024");
+
+        // sort by one course per term
+        List<List<String>> result = graph.topoSort(1);
+
+        // There should be 3 courses listed
+        assertEquals(3, result.size());
+
+        // Check that the courses are in the correct order
+        assertEquals("COMP1043", result.get(0).get(0));
+        assertEquals("COMP1048", result.get(1).get(0));
+        assertEquals("INFT1024", result.get(2).get(0));
+
+
+
     }
+
+    @Test(expected = RuntimeException.class)
+    public void topoSortCycleException() {
+        graph.addVertex("COMP1043");
+        graph.addVertex("COMP1048");
+
+        // add edges ina cycle
+        graph.addEdge("COMP1043", "COMP1048");
+        graph.addEdge("COMP1048", "COMP1043");
+
+        // This should throw an exception
+        graph.topoSort(1);
+
+        // Baeldung, March 2025, Assert an Exception Is Thrown in JUnit 4 and 5,
+        // https://www.baeldung.com/junit-assert-exception
+    }
+
+
 }
