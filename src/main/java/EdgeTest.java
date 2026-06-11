07c1363974d3beb188496a1ea93387280931929c
AngRod15 <angie.rodley@student.adelaide.edu.au>
2026-06-11 13:47:09 +1000

    Implemented tests for all methods

--- src/main/java/EdgeTest.java
+++ src/main/java/EdgeTest.java
@@ -1,28 +1,63 @@
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+/**
+ * A class to test the methods in the Edge class
+ * **/
 public class EdgeTest {
 
+    // Create edges to use in tests
+    private Edge edge1;
+    private Edge edge2;
+    private Edge edge3;
+
+    @Before
+    public void setUp() {
+        edge1 = new Edge("INFT1024", "INFT1026");
+        edge2 = new Edge("COMP1043", "INFT1032");
+        edge3 = new Edge("INFT1024", "INFT1026"); // Same as edge 1
+    }
+
+    // Test the getFrom method
     @Test
     public void getFrom() {
+        assertEquals("INFT1024", edge1.getFrom());
     }
 
+    // Test the getTo method
     @Test
     public void getTo() {
+        assertEquals("INFT1032", edge2.getTo());
     }
 
+    // Test the getWeight method
     @Test
     public void getWeight() {
+        assertEquals(1, edge1.getWeight());
     }
 
+    // Test the toString method
     @Test
     public void testToString() {
+        assertEquals("Edge(INFT1024 -> INFT1026, weight = 1)", edge1.toString());
     }
 
+    // Test the equals Override method
+    // Confirm Equals and Not Equals
     @Test
     public void testEquals() {
+        assertEquals(edge1, edge3);
+        assertNotEquals(edge1, edge2);
     }
 
+    // Test the HashCode override
+    // Confirm Equals and NotEquals
     @Test
     public void testHashCode() {
+        assertEquals(edge1.hashCode(), edge3.hashCode());
+        assertNotEquals(edge1.hashCode(), edge2.hashCode());
     }
 }
--- src/main/java/VertexTest.java
+++ src/main/java/VertexTest.java
@@ -1,20 +1,51 @@
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+/**
+ * A class to test the methods in the Vertex class
+ **/
 public class VertexTest {
 
+    // Create some vertices to use in tests
+    private Vertex vertex1;
+    private Vertex vertex2;
+    private Vertex vertex3;
+
+    @Before
+    public void setUp() {
+        vertex1 = new Vertex("INFT1024");
+        vertex2 = new Vertex("COMP1043");
+        vertex3 = new Vertex("INFT1024"); // Same as vertex1
+    }
+
+    // Test the getCourseCode method
     @Test
     public void getCourseCode() {
+        assertEquals("INFT1024", vertex1.getCourseCode());
     }
 
+    // test the toString override
     @Test
     public void testToString() {
+        assertEquals("Vertex(Course Code = INFT1024)", vertex1.toString());
     }
 
+    // test the hashCode override
+    // Confirm equals and not equals
     @Test
     public void testHashCode() {
+        assertEquals(vertex1.hashCode(), vertex3.hashCode());
+        assertNotEquals(vertex1.hashCode(), vertex2.hashCode());
     }
 
+    // Test the equals override
+    // Confirm Equals and Not Equals
     @Test
     public void testEquals() {
+        assertEquals(vertex1, vertex3);
+        assertNotEquals(vertex1, vertex2);
     }
 }
